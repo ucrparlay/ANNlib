a051e4cef5b12c5fb57cb1ef0e53bcfdf17093f1
Zheqi Shen <zheqi.shen@email.ucr.edu>
2024-04-28 19:17:33 -0700

    Implement shared_vector
    ptr_adapter: wrap a smart pointer to meet the requirements of Allocator::pointer
    shared_allocator: make the allocation of the underlying allocator shareable by applying a shared_ptr to it
    shared_vector: a sequential container with shared storage allowing to share a limited prefix between the copies

--- test/cpam.hpp
+++ test/cpam.hpp
@@ -5,15 +5,257 @@
 #include <tuple>
 #include <unordered_map>
 #include <memory>
+#include <functional>
 #include <utility>
 #include <type_traits>
 #include <parlay/parallel.h>
 #include <parlay/primitives.h>
 #include "util/util.hpp"
+#include "util/iter.hpp"
 #include "util/seq.hpp" // TODO: remove once switch to C++20
 #include "custom/custom.hpp"
 #include "pam/pam.h"
 #include "cpam/cpam.h"
+
+struct inner_t{};
+
+// Wrap a (fancy) pointer type UPtr to meet the requirements of A::pointer
+// where A is an Allocator following the C++ named requirements
+template<class UPtr>
+class ptr_adapter : 
+	public UPtr,
+	public ANN::util::enable_rand_iter<
+		ptr_adapter<UPtr>, size_t, ANN::util::empty>
+{
+	using base_ptr = UPtr;
+	using base_iter = ANN::util::enable_rand_iter<
+		ptr_adapter<UPtr>, size_t, ANN::util::empty
+	>;
+	using T = typename UPtr::element_type;
+
+public:
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+
+	ptr_adapter() : base_ptr(), base_iter(0){
+	}
+	ptr_adapter(const ptr_adapter&) = default;
+	ptr_adapter(ptr_adapter&&) = default;
+	template<typename ...Args>
+	ptr_adapter(inner_t, size_t offset, Args &&...args) :
+		base_ptr(std::forward<Args>(args)...),
+		base_iter(offset){
+	}
+	template<typename A, typename ...Args, typename=std::enable_if_t<!std::is_same_v<
+		std::remove_cv_t<std::remove_reference_t<A>>,
+		ptr_adapter<typename std::pointer_traits<UPtr>::template rebind<std::remove_cv_t<T>>>
+	>>>
+	ptr_adapter(A &&a, Args &&...args) : 
+		ptr_adapter(inner_t{}, size_t(0), std::forward<A>(a), std::forward<Args>(args)...){
+	}
+	ptr_adapter& operator=(const ptr_adapter&) = default;
+	ptr_adapter& operator=(ptr_adapter&&) = default;
+	/*
+	ptr_adapter(UPtr p) : 
+		base_ptr(std::move(p)),
+		base_iter(0){
+	}
+	*/
+
+	pointer operator->() const{
+		return base_ptr::operator->()+base_iter::get_pos();
+	}
+	reference operator*() const{
+		return *operator->();
+	}
+	operator T*() const{
+		return operator->();
+	}
+
+	// TODO: use the default comparison in C++20
+	// bool operator==(const ptr_adapter &rhs) const = default;
+	bool operator==(const ptr_adapter &rhs) const{
+		return static_cast<const base_ptr&>(*this) ==
+			static_cast<const base_ptr&>(rhs) &&
+				static_cast<const base_iter&>(*this) ==
+			static_cast<const base_iter&>(rhs);
+	}
+	bool operator!=(const ptr_adapter &rhs) const{
+		return !operator==(rhs);
+	}
+	bool operator==(const nullptr_t &null) const{
+		return base_iter::get_pos()==0 &&
+			static_cast<const base_ptr&>(*this)==null;
+	}
+	bool operator!=(const nullptr_t &null) const{
+		return !operator==(null);
+	}
+
+	template<typename U=T, class CUPtr=std::enable_if_t<
+		!std::is_const_v<U>,
+		typename std::pointer_traits<UPtr>::template rebind<const U>
+	>>
+	operator ptr_adapter<CUPtr>() const{
+		return {inner_t{}, base_iter::get_pos(), static_cast<const base_ptr&>(*this)};
+	}
+};
+
+template<class Alloc>
+class shared_allocator : public Alloc{
+	using at = std::allocator_traits<Alloc>;
+
+	static_assert(std::is_pointer_v<typename at::pointer>);
+	using sz_t = typename at::size_type;
+	using val_t = typename at::value_type;
+
+	Alloc& base(){
+		return static_cast<Alloc&>(*this);
+	}
+	const Alloc& base() const{
+		return static_cast<const Alloc&>(*this);
+	}
+
+public:
+	using pointer = ptr_adapter<std::shared_ptr<val_t>>;
+	using const_pointer = ptr_adapter<std::shared_ptr<const val_t>>;
+	using Alloc::Alloc;
+
+	template<typename ...Args>
+	pointer allocate(sz_t n, Args ...args){
+		return pointer(
+			Alloc::allocate(n, std::forward<Args>(args)...),
+			[=](auto *p){this->Alloc::deallocate(p,n);},
+			base()
+		);
+	}
+
+	void deallocate(pointer &p, sz_t /*n*/){
+		p.reset();
+	}
+	void deallocate(pointer &&/*p*/, sz_t /*n*/){
+		// we do nothing on p as it is destroyed after the call
+	}
+
+	template<typename U>
+	struct rebind{
+		using other = shared_allocator<typename at::template rebind_alloc<U>>;
+	};
+
+	bool operator==(const shared_allocator &rhs) const{
+		return base()==rhs.base();
+	}
+	bool operator!=(const shared_allocator &rhs) const{
+		return base()!=rhs.base();
+	}
+};
+
+
+template<typename T>
+class shared_vector
+{
+	using cm = ANN::custom<typename ANN::lookup_custom_tag<T>::type>;
+	using alloc_t = shared_allocator<typename cm::alloc<T>>;
+	using ptr_t = typename alloc_t::pointer;
+	ptr_t storage;
+	size_t len, cap;
+	alloc_t alloc;
+	static const constexpr float over_resv = 1.6;
+
+	template<typename V>
+	void push_back_impl(V &&value){
+		if(len<cap){
+			new(end()) T(std::forward<V>(value));
+			len++;
+			return;
+		}
+		size_t cap_new = (len+1)*over_resv;
+		ptr_t s_new = alloc.allocate(cap_new);
+		T *p = s_new.operator->();
+		for(auto it=begin(); it!=end(); ++it, ++p)
+			new(p) T(*it);
+		new(p) T(std::forward<V>(value));
+		len++;
+		if(storage!=nullptr)
+			alloc.deallocate(storage, cap);
+		storage = std::move(s_new);
+		cap = cap_new;
+	}
+public:
+	typedef ptr_t pointer;
+	typedef T value_type;
+	typedef T* iterator;
+	typedef const T* const_iterator;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+
+	shared_vector() : len(0), cap(0){
+	}
+	template<typename Iter>
+	shared_vector(Iter start, Iter last) : 
+		len(std::distance(start, last)), cap(len*over_resv)
+	{
+		if(len==0) return;
+
+		storage = alloc.allocate(cap);
+		T *p = storage.operator->();
+		for(; start!=last; ++start, ++p)
+			new(p) T(*start);
+	}
+	shared_vector(shared_vector&&) noexcept = default;
+	shared_vector(const shared_vector&) = default;
+	shared_vector& operator=(shared_vector&&) noexcept = default;
+	shared_vector& operator=(const shared_vector&) = default;
+
+	T* data() noexcept{
+		return storage.operator->();
+	}
+	const T* data() const noexcept{
+		return storage.operator->();
+	}
+	size_type size() const noexcept{
+		return len;
+	}
+	size_type capacity() const noexcept{
+		return cap;
+	}
+	iterator begin() noexcept{
+		return data();
+	}
+	const_iterator begin() const noexcept{
+		return data();
+	}
+	iterator end() noexcept{
+		return data()+len;
+	}
+	const_iterator end() const noexcept{
+		return data()+len;
+	}
+	reference operator[](size_type pos){
+		return data()[pos];
+	}
+	const_reference operator[](size_type pos) const{
+		return data()[pos];
+	}
+	void push_back(const T &value){
+		push_back_impl(value);
+	}
+	void push_back(T &&value){
+		push_back_impl(std::move(value));
+	}
+	void clear(){
+		if(storage!=nullptr)
+			alloc.deallocate(storage, cap);
+		len = cap = 0;
+	}
+
+	~shared_vector(){
+		clear();
+	}
+};
+
 
 template<typename Nid, class Ext, class Edge=Nid>
 class graph_cpam : ANN::graph::base
@@ -24,7 +266,11 @@ public:
 
 private:
 	using cm = ANN::custom<typename ANN::lookup_custom_tag<Nid>::type>;
-	using edgelist = typename cm::seq<Edge>;
+	// using edgelist = typename cm::seq<Edge>;
+	// TODO: make parlay::sequence support Allocator::pointer
+	// using edgelist = std::vector<Edge, shared_allocator<typename cm::alloc<Edge>>>;
+	// using edgelist = std::vector<Edge>;
+	using edgelist = shared_vector<Edge>;
 
 	// vinfo_* are designed to be reference-counted so that
 	// copying it over multiple versions is efficient
